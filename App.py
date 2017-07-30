c25df532f7669aa40085d5ddbf00f595d24d073b
mpimblott <matthew@pimblott.com>
2017-07-30 20:40:59 +0100

    Login Screen

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
@@ -10,6 +10,17 @@ def main():
 @app.route('/hello/')
 def hello():
     return render_template('template.html', my_string="Go away!", my_list=[0, 1, 2, 3, 4, 5])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('hello'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     # app.debug = True
