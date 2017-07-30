5396de0f9c64f05357d7ba7b5d05f4670b9cc0e0
mpimblott <matthew@pimblott.com>
2017-07-30 20:04:26 +0100

    my first file

--- App.py
+++ App.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def main():
-    return 'Hello World'
+    return render_template('template.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5])
+
+
+@app.route('/hello/')
+def hello():
+    return render_template('template.html', my_string="Go away!", my_list=[0, 1, 2, 3, 4, 5])
 
 if __name__ == '__main__':
     # app.debug = True
