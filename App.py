783868a49069f813beb227cc5351dcd7a842c254
mpimblott <matthew@pimblott.com>
2017-07-30 19:44:51 +0100

    my first file

---
+++ App.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def main():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run()
+    # app.run(debug=True)
