297082a460cd2b4d49e81e2b00986df73c507fd9
Gordon Pimblott <g.pimblott@kainos.com>
2017-07-30 21:09:37 +0100

    run under gunicorn

--- App.py
+++ App.py
@@ -22,7 +22,7 @@ def login():
             return redirect(url_for('hello'))
     return render_template('login.html', error=error)
 
-if __name__ == '__main__':
-    # app.debug = True
+
+def startup():
     app.run()
-    # app.run(debug=True)
+
