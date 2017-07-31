cadd18cf34c580f22106f99dc42bdef4b182c585
mpimblott <matthew@pimblott.com>
2017-07-31 09:46:43 +0100

    CSS working

--- myProject.py
+++ myProject.py
@@ -8,7 +8,7 @@ def home():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     else:
-        return render_template('test')
+        return render_template('index.html')
 
 
 @app.route('/hello')
@@ -16,23 +16,28 @@ def hello():
     return render_template('test', my_string="Go away!", my_list=[0, 1, 2, 3, 4, 5])
 
 
-@app.route('/home')
-def main():
-    return render_template('index')
+@app.route('/boot')
+def boot():
+    return render_template('boot.html', my_string="Go away!", my_list=[0, 1, 2, 3, 4, 5])
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        if not session.get('logged_in'):
+            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                session['logged_in'] = True
+                return redirect(url_for('home'))
         else:
-            session['logged_in'] = True
-            return redirect(url_for('/'))
+            error = 'already logged in'
+
     return render_template('/login.html', error=error)
 
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
     app.run()
 
