55081285c2938bec4440805d00f061242205f4bf
mpimblott <matthew@pimblott.com>
2017-07-31 07:42:24 +0100

    CSS working

--- myProject.py
+++ myProject.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request # send_from_directory
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
@@ -12,6 +12,16 @@ def hello():
     return render_template('template.html', my_string="Go away!", my_list=[0, 1, 2, 3, 4, 5])
 
 
+@app.route('/test')
+def test():
+    return render_template('test.html', my_string="Go away!", my_list=[0, 1, 2, 3, 4, 5])
+
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -20,7 +30,7 @@ def login():
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('hello'))
-    return render_template('login.html', error=error)
+    return render_template('/login.html', error=error)
 
 
 if __name__ == "__main__":
