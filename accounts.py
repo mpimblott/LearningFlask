afc5c0ef3c73b10a60c4051d188db5ae70fd0874
mpimblott <matthew@pimblott.com>
2017-08-01 16:43:13 +0100

    added tags to header on index

--- accounts.py
+++ accounts.py
@@ -1,12 +1,12 @@
 import datetime
 from datetime import datetime
-import os
+# import os
 
-highest_id = 1
+# highest_id = 1
 
 
 class Account(object):
-    id = os.urandom(6)
+    # id = os.urandom(6)
     name = ""
     password = ""
     is_admin = False
@@ -18,6 +18,7 @@ class Account(object):
 
 def create_account(name, password, is_admin):
     account = Account()
+    # account.id = highest_id += 1
     account.name = name
     account.password = password
     account.is_admin = is_admin
